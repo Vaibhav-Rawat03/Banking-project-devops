a8428ed50ec97d62295c59f90eea2d42a9ee7356
vaibhav <vaibhavrawat2003@gmail.com>
2022-10-10 12:05:19 +0530

    Pushed by vaid

--- Banking.java
+++ Banking.java
@@ -15,5 +15,20 @@ class Banking{
 
         return balance;
     }
-
+    void getInfo(){
+        System.out.println("Enter the Name");
+        this.name=sc.nextLine();
+        System.out.println("Enter the Age");
+        this.age=sc.nextInt();
+        System.out.println("Enter the Aadhar no.");
+        this.aadhar=sc.next();
+    }
+    void displayInfo(name){
+        try{
+        System.out.println("Name= "+name\n+"Age="+age\n+"Aadhar no.="+aadhar\n+"Balance="+balance);
+        }
+        except{
+            System.out.println("Name not found");
+        }
+    }
 }
